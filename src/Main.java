ac1af9ff814da7592679ffb87cba181abebf83e1
Dasha <d.5ysueva@yandex.ru>
2025-04-17 11:15:31 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,20 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String reg1= "/\\*(([a-zA-Z0-9])*|(\\s)*)|(([a-zA-Z0-9])*|(\\s)*)\\*/";
+        String reg1= "/\\*(.*|(\\s)*)|(.*|(\\s)*)\\*/$";
         String reg2= "//.*";
-        Pattern p1 = Pattern.compile(reg1, Pattern.DOTALL);
+        Pattern p1 = Pattern.compile(reg1);
         Pattern p2 = Pattern.compile(reg2);
         try(BufferedReader br = new BufferedReader(new FileReader("Lab.txt"));
         BufferedWriter bt = new BufferedWriter( new FileWriter("rez.txt"))){
             String text;
 
-        while ((text = br.readLine()) != null) {
-
-
+            while ((text = br.readLine()) != null) {
             text= p1.matcher(text).replaceAll("");
             text =p2.matcher(text).replaceAll("");
             bt.write(text);
             bt.newLine();
-        }
+            }
         }
         catch (IOException ex){
             System.out.println(ex.getMessage());
